f2e25e23dfbaf98175b8f9614de9823ae391873d
AlexChronos1806 <alex.afa1806@gmail.com>
2018-02-08 17:01:38 -0200

    primeiro commit

---
+++ app/src/main/java/fiap/com/br/dailywork/AboutActivity.java
@@ -0,0 +1,16 @@
+package fiap.com.br.dailywork;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+
+}
